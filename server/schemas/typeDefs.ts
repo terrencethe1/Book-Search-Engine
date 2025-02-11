adb0d9f23548c513b30c6b2490f58c4ee8a8d8fb
terrencethe1 <terrence.jsr@gmail.com>
2025-02-10 23:04:59 -0600

    add GraphQL type definitions for user and book management in the typeDefs.ts file

---
+++ server/schemas/typeDefs.ts
@@ -0,0 +1,44 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+  }
+
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+export default typeDefs;
+
+
