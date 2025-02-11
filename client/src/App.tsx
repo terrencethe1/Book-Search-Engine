4a1efa70ba463544a398e9dde67667bccb17a54b
terrencethe1 <terrence.jsr@gmail.com>
2025-02-11 13:21:39 -0600

    set up Apollo Client and integrate with React components in the app.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,46 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Create an HTTP link to the GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',  // Assuming the server runs on the same domain
+});
+
+// Set the Authorization header for requests
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create the Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+
+
