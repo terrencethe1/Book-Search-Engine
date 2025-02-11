a669908039b7e1f0e3ab466a51eff77efe74fc73
terrencethe1 <terrence.jsr@gmail.com>
2025-02-10 22:48:03 -0600

    add startApolloServer function to initialize Apollo Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,4 +21,11 @@ const PORT = process.env.PORT || 3001;\
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const startApolloServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+
+
 
