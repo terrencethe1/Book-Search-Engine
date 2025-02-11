3cfc63a0a2e0b89e2fc18c102036ed9767c81008
terrencethe1 <terrence.jsr@gmail.com>
2025-02-10 22:40:36 -0600

    update import statements for Apollo Server and middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 import path from 'node:path';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { authMiddleware } from './services/auth.js';
+
+import { fileURLToPath } from 'url';
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
