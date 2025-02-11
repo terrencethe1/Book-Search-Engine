966dcb64366e7c1235d1fc6412bf448e04d24ef4
terrencethe1 <terrence.jsr@gmail.com>
2025-02-10 22:43:19 -0600

    add __filename and __dirname constants for module path resolution

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,19 +12,5 @@ import { fileURLToPath } from 'url';
 
 
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
